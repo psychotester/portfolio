7bd8499d3cfd1b7922be7f973dc1c0ceb16447e1
Oskar Rubin <oskar.rubin.box@gmail.com>
2026-01-03 16:29:29 -0800

    modify styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error('Root element "#root" not found in public/index.html');
+}
+
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
